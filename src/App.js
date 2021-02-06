001e855914da3b849bc9607fdbc9860c8153b205
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-06 10:53:37 +0600

    App.js title changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Shop from "./components/Shop/Shop";
 function App() {
   return (
     <div>
-      <h1 className="text-center badge-primary">Simple Redux Cart</h1>
+      <h1 className="text-center badge-primary">Simple React Redux Cart</h1>
       <Cart />
       <Shop />
     </div>
